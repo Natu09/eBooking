ff1164266b3740236a1de323c4ce765fea87ffb3
[Natu09] <[nathaniel.habtegerge@ucalgary.ca]>
2019-11-22 12:42:45 -0700

    "made a gitignore and initialled connection to DB"

---
+++ server/knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    version: 11.5,
+    connection: {
+      host: 'e-booking-db.cfwfxw7xy4fu.us-east-2.rds.amazonaws.com',
+      user: 'cpsc_471',
+      password: 'e-booking19',
+      database: 'ebooking_DB'
+    }
+  },
+
+};
