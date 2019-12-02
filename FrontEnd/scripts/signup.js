3c09db87d1e5e0c7284631a9f8a59de36dd52a9f
NatuO9 <nathaniel.habtegerge@ucalgary.ca>
2019-12-01 21:38:10 -0700

    "created login authentication for users:wq"

---
+++ FrontEnd/scripts/signup.js
@@ -0,0 +1,49 @@
+$(() => {
+    console.log('Hello from sign up jQueer')
+    $('form').submit((event) => {
+        event.preventDefault();
+        console.log('submited!!');
+        const fname = $('#fname').val();
+        const lname = $('#lname').val();
+        const email = $('#email').val();
+        const password = $('#password').val();
+        const user = {
+            fname,
+            lname,
+            email,
+            password
+        }
+
+        signup(user)
+            .then(result => {
+                console.log(result)
+                window.location = `/user_dashboard.html?id=${result.id}`
+            })
+    
+            .catch(error => {
+                console.error(error)
+                $errorMessage = $('#errorMessage')
+                $errorMessage.text(error.responseJSON.message)
+                $errorMessage.show()
+            })
+
+    });
+});
+
+function signup(user) {
+    return $.post(`${AUTH_URL}/auth/signup`, user);
+}
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
